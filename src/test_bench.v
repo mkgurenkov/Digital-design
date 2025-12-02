f58a38ccd5c9fdc05cb94e7f0118b417dd7100e8
Maksim Gurenkov <git@smashingalpha.com>
2025-12-03 00:09:45 +0300

    add one more test

--- src/test_bench.v
+++ src/test_bench.v
@@ -26,9 +26,11 @@ module test_bench;
         $dumpfile("wave.vcd");
         $dumpvars(0, test_bench);
 
-        test(8'd5, 8'd27, 11'd15,  1); // 5 * cb
+        test(8'd5, 8'd27, 11'd15,  1);
         test(8'd3, 8'd64, 11'd12,  2);
         test(8'd9,   8'd125,  11'd45,  3);
+
+        test(8'd255,   8'd200,  11'd1275,  3);
 
         #10 $finish;
     end
